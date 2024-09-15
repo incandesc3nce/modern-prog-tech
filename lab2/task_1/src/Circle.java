36ef7224bbce5da3296b953465208d7966c8b777
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-09-15 19:16:56 +0700

    lab2(java): solve task 1

--- lab2/task_1/src/Circle.java
+++ lab2/task_1/src/Circle.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class Circle {
+    private double radius;
+    private double x;
+    private double y;
+
+    public void init(double r, double arg_x, double arg_y) {
+        radius = r;
+        x = arg_x;
+        y = arg_y;
+    }
+
+    public void read() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите радиус: ");
+        radius = sc.nextDouble();
+        System.out.print("Введите координату x: ");
+        x = sc.nextDouble();
+        System.out.print("Введите координату y: ");
+        y = sc.nextDouble();
+    }
+
+    public void display() {
+        System.out.println("Радиус: " + radius);
+        System.out.println("Координата x: " + x);
+        System.out.println("Координата y: " + y);
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Circle add(Circle c1, Circle c2) {
+        Circle result = new Circle();
+        result.init(c1.radius + c2.radius, c1.x + c2.x, c1.y + c2.y);
+        return result;
+    }
 }
--- lab2/task_1/src/Main.java
+++ lab2/task_1/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle[] circleArray = new Circle[3];
 
+        for (int i = 0; i < circleArray.length; i++) {
+            circleArray[i] = new Circle();
+        }
+
+        c1.init(5, 1, 2);
+        c1.display();
+        System.out.println(c1.distance());
+
+        for (Circle circle : circleArray) {
+            circle.read();
+            circle.display();
+        }
+
+        Circle c2 = new Circle();
+        c2.init(0, 0, 0);
+
+        for (Circle circle : circleArray) {
+            c2 = c2.add(c2, circle);
+        }
+
+        c2.display();
     }
 }
