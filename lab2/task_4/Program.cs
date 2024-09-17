7be0e06a466e8aad574bcc059b10bfe1cce8dc5d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-09-17 13:26:18 +0700

    lab2: solve task_4

---
+++ lab2/task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JewelryShop shop = new JewelryShop();
+
+            shop.Read();
+
+            shop.Display();
+
+            Console.WriteLine("Общая стоимость всех украшений: " + shop.GetFullPrice());
+
+            Console.WriteLine("Самое дорогое украшение:");
+            shop.MostExpensiveJewelry().Display();
+        }
+    }
+}
