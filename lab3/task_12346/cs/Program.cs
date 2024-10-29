564fd2387e49858abcf2664abc97e1b4dd34efa0
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-10-29 12:24:35 +0700

    lab3: add comments for ref and out

--- lab3/task_12346/cs/Program.cs
+++ lab3/task_12346/cs/Program.cs
@@ -15,15 +15,15 @@ namespace Lab3 {
       added.Display();
 
       double distance = added.Distance();
-      double reff = 0;
-      double outt = 0;
+      double distanceRef = 0; // ининциализация для ref обязательна
+      double distnaceOut; // инициализация для out не обязательна
 
-      added.CircleRef(ref reff);
-      added.CircleOut(out outt);
+      added.CircleRef(ref distanceRef); // выдаст ошибку если не инициализировать ref
+      added.CircleOut(out distnaceOut);
       
       Console.WriteLine("Расстояние: " + distance);
-      Console.WriteLine("Расстояние через ref: " + reff);
-      Console.WriteLine("Расстояние через out: " + outt);
+      Console.WriteLine("Расстояние через ref: " + distanceRef);
+      Console.WriteLine("Расстояние через out: " + distnaceOut);
 
       Circle circle3 = Circle.Add(circle1, circle2);
       Console.WriteLine("Два аргумента: ");
@@ -39,12 +39,12 @@ namespace Lab3 {
       Console.WriteLine("Расстояние через статический метод: ");
       Console.WriteLine(Circle.StaticDistance(circle2));
 
-      double result;
+      double result = 0;
       try {
         circle1.CircleRef(ref result);
         Console.WriteLine("Расстояние через ref: " + result);
-      } catch (Exception e) {
-        Console.WriteLine(e.Message);
+      } catch (Exception ex) {
+        Console.WriteLine(ex.Message);
       }
     }
   }
---
+++ lab3/task_5/cpp/classes/Jewelry.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#ifndef JEWELRY_H
+#define JEWELRY_H
+using namespace std;
+
+class Jewelry {
+  private: 
+    double weight;
+    double pricePerGramm;
+
+  public:
+    void init(double arg_weight, double arg_pricePerGramm);
+
+    void read();
+
+    void display();
+
+    double getFullPricePerGramm();
+};
+
+  void Jewelry::init(double arg_weight, double arg_pricePerGramm) {
+    weight = arg_weight;
+    pricePerGramm = arg_pricePerGramm;
+  }
+
+  void Jewelry::read() {
+    cout << "Введите вес в граммах: ";
+    cin >> weight;
+    cout << "Введите цену за грамм: ";
+    cin >> pricePerGramm;
+  }
+
+  void Jewelry::display() {
+    cout << "Вес: " << weight << endl;
+    cout << "Цена за грамм: " << pricePerGramm << endl;
+  }
+
+  double Jewelry::getFullPricePerGramm() {
+    return weight * pricePerGramm;
+  }
+
+#endif
