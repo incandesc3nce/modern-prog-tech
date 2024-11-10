fddaf7e4cfb45a836aef46cb68124debf8512d2e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-11-10 20:57:58 +0700

    lab4: solve task 2 cs

---
+++ lab4/cs/Lab4_2/Jewelry.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace Lab4_2
+{
+    public class Jewelry
+    {
+        private double weight = 1;
+        private double pricePerGramm = 1;
+
+        public void Init(double arg_weight, double arg_pricePerGramm)
+        {
+            weight = arg_weight;
+            pricePerGramm = arg_pricePerGramm;
+        }
+
+        public void Read()
+        {
+            Console.Write("Введите вес в граммах: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите цену за грамм: ");
+            pricePerGramm = Convert.ToDouble(Console.ReadLine());
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Цена за грамм: {pricePerGramm}");
+        }
+
+        public double GetFullPricePerGramm()
+        {
+            return weight * pricePerGramm;
+        }
+
+        public static Jewelry operator +(Jewelry j1, Jewelry j2) {
+            return new Jewelry
+            {
+                weight = j1.weight + j2.weight,
+                pricePerGramm = (j1.pricePerGramm + j2.pricePerGramm) / 2
+            };
+        }
+
+        public static Jewelry operator +(Jewelry j, double val) {
+            return new Jewelry
+            {
+                weight = j.weight + val,
+                pricePerGramm = j.pricePerGramm
+            };
+        }
+
+        public static Jewelry operator +(double val, Jewelry j) {
+            return new Jewelry
+            {
+                weight = j.weight + val,
+                pricePerGramm = j.pricePerGramm
+            };
+        }
+
+        public static Jewelry operator ++(Jewelry j) {
+            ++j.pricePerGramm;
+            return j;
+        }
+    }
+}
---
+++ lab4/cs/Lab4_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jewelry j1 = new Jewelry();
+            j1.Init(10, 100);
+
+            Jewelry j2 = new Jewelry();
+            j2.Init(5, 50);
+
+            Jewelry j3 = j1 + j2;
+            Console.WriteLine("Сложение двух объектов:");
+            j3.Display();
+
+            Console.WriteLine("Сложение объекта и числа:");
+            j3 = j1 + 5;
+            j3.Display();
+
+            j3 = 10 + j1;
+            j3.Display();
+
+            Console.WriteLine("Сложение 2 объектов и числа:");
+            j3 = j1 + j2 + 5;
+            j3.Display();
+            
+            j1.Init(10, 100);
+            j3 = j1++;
+            Console.WriteLine("Постфиксный инкремент:");
+            j3.Display();
+
+            j1.Init(10, 100);
+            j3 = ++j1;
+            Console.WriteLine("Префиксный инкремент:");
+            j3.Display();
+        }
+    }
+}
