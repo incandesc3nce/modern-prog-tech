3adac55cb0c731296f235a0d8d93050310c79b25
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-11-10 16:10:03 +0700

    lab4: solve tasks 1 and 2 cpp

---
+++ lab4/cpp/1/Circle.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+  private:
+    double radius;
+    double x;
+    double y;
+
+  public:
+    Circle();
+    Circle(double r);
+    Circle(double r, double x, double y);
+
+    void Init(double r, double x, double y);
+    void Read();
+    void Display();
+
+    double distance();
+    void distance(double& d);
+    void distance(double* d);
+    static double staticDistance(Circle c);
+
+    static Circle staticAdd(Circle c1, Circle c2);
+    Circle add(Circle c2);
+
+    friend double nearestPointDistance(Circle c);
+
+    Circle operator+(Circle c);
+    Circle operator+(double val);
+
+    Circle& operator++();
+    Circle operator++(int unused);
+};
+
+  Circle::Circle() {
+    this->Init(0, 0, 0);
+  }
+
+  Circle::Circle(double r) {
+    this->Init(r, 0, 0);
+  }
+
+  Circle::Circle(double r, double x, double y) {
+    this->Init(r, x, y);
+  }
+
+  void Circle::Init(double r, double arg_x, double arg_y) {
+    radius = r;
+    x = arg_x;
+    y = arg_y;
+  }
+
+  void Circle::Read() {
+    cout << "Введите радиус:";
+    cin >> radius;
+    cout << "Введите координату x:";
+    cin >> x;
+    cout << "Введите координату y:";
+    cin >> y;
+  }
+
+  void Circle::Display() {
+    cout << "Радиус: " << radius << endl;
+    cout << "Координата x: " << x << endl;
+    cout << "Координата y: " << y << endl;
+  }
+
+  double Circle::distance() {
+    try {
+      if (radius < 0) {
+        throw "Радиус не может быть отрицательным";
+      }
+      if (radius == 0) {
+        throw "Радиус не может быть равен нулю";
+      }
+    } catch (const char* msg) {
+      cout << msg << endl;
+      return -1;
+    }
+
+    return sqrt(x * x + y * y);
+  }
+
+  void Circle::distance(double& d) {
+    d = distance();
+  }
+
+  void Circle::distance(double* d) {
+    *d = distance();
+  }
+
+  double Circle::staticDistance(Circle c) {
+    return c.distance();
+  }
+
+  Circle Circle::staticAdd(Circle c1, Circle c2) {
+    Circle result;
+    result.Init(c1.radius + c2.radius, c1.x + c2.x, c1.y + c2.y);
+    return result;
+  }
+
+  Circle Circle::add(Circle c2) {
+    this->radius = this->radius + c2.radius;
+    this->x = this->x + c2.x;
+    this->y = this->y + c2.y;
+
+    Circle result;
+    result.Init(this->radius, this->x, this->y);
+    return result;
+  }
+
+  double nearestPointDistance(Circle c) {
+    double distanceToCenter = sqrt(c.x * c.x + c.y + c.y);
+    
+    return distanceToCenter - c.radius;
+  }
+
+  Circle Circle::operator+(Circle c) {
+    Circle result;
+    result.x = this->x + c.x;
+    result.y = this->y + c.y;
+    result.radius = this->radius + c.radius;
+
+    return result;
+  }
+
+  Circle Circle::operator+(double val) {
+    Circle result;
+    result.radius = this->radius + val;
+    result.x = this->x;
+    result.y = this->y;
+
+    return result;
+  }
+
+  Circle &Circle::operator++() {
+    this->radius++;
+    return *this;
+  }
+
+  Circle Circle::operator++(int unused) {
+    Circle result = *this;
+    this->radius++;
+    return result;
+  }
---
+++ lab4/cpp/1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Circle.cpp"
+
+using namespace std;
+
+int main() {
+    Circle c1(5, 8, 3);
+
+    c1.Display();
+
+    double nearestDist = nearestPointDistance(c1);
+    cout << "Расстояние от начала координат до ближайшей точки окружности: " << nearestDist << endl;
+
+    Circle c2(2, 2, 1);
+    Circle c3 = c1 + c2;
+    cout << "Сложение двух объектов:" << endl;
+    c3.Display();
+
+    Circle c4 = c3 + 4;
+    cout << "Сложение объекта и числа:" << endl;
+    c4.Display();
+
+    c1.Init(5, 8, 3);
+
+    Circle c5 = ++c1;
+    cout << "После префиксного инкремента c1:" << endl;
+    c5.Display();
+
+    c1.Init(5, 8, 3);
+
+    c5 = c1++;
+    cout << "После постфиксного инкремента c1:" << endl;
+    c5.Display();
+
+    return 0;
+}
