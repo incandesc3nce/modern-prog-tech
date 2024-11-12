214529f6d58225f4267c633259b17c1d8eb5eed1
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-11-12 13:52:57 +0700

    lab4: solve tasks 5 and 6 cs

---
+++ lab4/cs/Lab4_1/Circle.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace Lab4_1 {
+public class Circle
+{
+    private double radius;
+    private double x;
+    private double y;
+
+    public double Radius {
+      get { return radius; }
+      set { radius = value; }
+    }
+
+    public void Init(double r, double arg_x, double arg_y)
+    {
+        Radius = r;
+        x = arg_x;
+        y = arg_y;
+    }
+
+    public void Read()
+    {
+        Console.Write("Введите радиус: ");
+        radius = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координату x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координату y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Радиус: {radius}");
+        Console.WriteLine($"Координата x: {x}");
+        Console.WriteLine($"Координата y: {y}");
+    }
+
+    public double GetX()
+    {
+        return x;
+    }
+
+    public double GetY()
+    {
+        return y;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+
+    public Circle Add(Circle c1, Circle c2)
+    {
+        Circle result = new Circle();
+        result.Init(c1.radius + c2.radius, c1.x + c2.x, c1.y + c2.y);
+        return result;
+    }
+}
+}
---
+++ lab4/cs/Lab4_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab4_1 {
+
+public class Program
+{ 
+  public static double Distance(double x, double y, double r)
+    {
+        return Math.Sqrt(x * x + y * y);
+    }
+
+  public static double nearestPointDistance(double x, double y, double r) {
+    double distanceToCenter = Math.Sqrt(x * x + y + y);
+    
+    return distanceToCenter - r;
+  }
+
+  public delegate double DistanceDelegate(double x, double y, double r);
+
+    public static void Main(string[] args)
+    {
+        DistanceDelegate distance = Distance;
+
+        Circle c1 = new Circle();
+        c1.Init(1, 4, 4);
+
+        double x = c1.GetX();
+        double y = c1.GetY();
+        double r = c1.GetRadius();
+
+        double distanceToCenter = distance(x, y, r);
+        distance = nearestPointDistance;
+        double distanceToNearestPoint = distance(x, y, r);
+
+        Console.WriteLine($"Расстояние до центра: {distanceToCenter}");
+        Console.WriteLine($"Расстояние до ближайшей точки: {distanceToNearestPoint}");
+    }
+}
+}
