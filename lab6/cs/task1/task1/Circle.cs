d984830ec3626054efb48327585b9486247819a3
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-12-17 17:29:21 +0700

    lab6: solve task 1 cs

---
+++ lab6/cs/task1/task1/Circle.cs
@@ -0,0 +1,82 @@
+using System;
+
+namespace task1
+{
+public class Circle
+{
+    protected double radius;
+    protected double x;
+    protected double y;
+
+    public double Radius {
+      get { return radius; }
+      set { radius = value; }
+    }
+
+    public void Init(double r, double arg_x, double arg_y)
+    {
+        Radius = r;
+        x = arg_x;
+        y = arg_y;
+    }
+
+    public void Read()
+    {
+        Console.Write("Введите радиус: ");
+        radius = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координату x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координату y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Радиус: {radius}");
+        Console.WriteLine($"Координата x: {x}");
+        Console.WriteLine($"Координата y: {y}");
+    }
+
+    public double Distance()
+    {
+        return Math.Sqrt(x * x + y * y);
+    }
+
+    public Circle Add(Circle c1, Circle c2)
+    {
+        Circle result = new Circle();
+        result.Init(c1.radius + c2.radius, c1.x + c2.x, c1.y + c2.y);
+        return result;
+    }
+
+    public Circle() {
+        radius = 0;
+        x = 0;
+        y = 0;
+    }
+
+    public Circle(double r) {
+        radius = r;
+        x = 0;
+        y = 0;
+    }
+
+    public Circle(double r, double arg_x, double arg_y) {
+        radius = r;
+        x = arg_x;
+        y = arg_y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
+}
---
+++ lab6/cs/task1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1 {
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cylinder c1 = new Cylinder();
+
+        c1.Init(1, 2, 3, 4);
+        c1.Display();
+
+        Console.WriteLine($"Расстояние от центра до точки: {c1.Distance()}");
+
+        c1 = new Cylinder(5);
+        c1.Display();
+
+        c1 = new Cylinder(6, 7, 8, 9);
+        c1.Display();
+        Console.WriteLine($"Расстояние от центра до точки: {c1.Distance()}");
+    }
+}
+}
